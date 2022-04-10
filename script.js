70b0a3c31eec600e41ca90d801b4fc8f9e5c8f80
jonasccosta <jonas-costa72@hotmail.com>
2022-04-10 11:16:43 -0500

    Added title, icons, and language flags to pages

--- script.js
+++ script.js
@@ -2,15 +2,10 @@ var menu = document.getElementById("main_menu").children;
 
 var file_name = location.pathname.split("/").slice(-1);
 
-console.log(file_name[0])
-for (var i = 0; i < menu.length; i++) {
-  var cell = menu[i];
-  console.log(cell.textContent);
-  console.log(file_name[0].slice(0, -5))
-  if (cell.textContent.toLowerCase() == file_name[0].slice(0, -5)) {
-    cell.style.color = "#A239CA";
-  } 
-  else{
-      console.log("HELP")
-  }
-}
+var file_name_edited = file_name[0].slice(0, -5)
+
+var menu_item_name = ["index", "about_me", "projects", "hobbies", "contact"];
+
+var current_page_index = menu_item_name.indexOf(file_name_edited);
+
+menu[current_page_index].style.color = "#A239CA";
